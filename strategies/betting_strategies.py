1f628a45af19616e0076c8756db0d419f4e1f670
Jakub Binkowski <jkb.binkowski@gmail.com>
2025-11-04 00:56:39 +0100

    Add MAX_BET setting to config and update minimal_bet function to enforce max bet limit

--- strategies/betting_strategies.py
+++ strategies/betting_strategies.py
@@ -2,5 +2,7 @@ def minimal_bet(player, game):
     bet = game.min_bet
     if bet > player.capital:
         raise ValueError("Bet is greater than capital")
+    elif bet > game.max_bet:
+        raise ValueError("Bet is greater than max bet")
     player.capital -= bet
     player.bets.append(bet)
