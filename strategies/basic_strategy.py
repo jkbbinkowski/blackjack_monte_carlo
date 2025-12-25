a36d3512558ab311580dc7a0951f040fa5bf326a
Jakub Binkowski <jkb.binkowski@gmail.com>
2025-12-25 11:31:08 +0100

    Refactor evaluate_move function in basic_strategy to improve handling of double down and surrender moves; clarify logic for soft 17 rule and ensure proper evaluation of player hands.

--- strategies/basic_strategy.py
+++ strategies/basic_strategy.py
@@ -1,34 +1,39 @@
 # evaluate basic strategy move based on tables and rules
 def evaluate_move(game, player, hand_idx, move):
+    # evaluate move based of S17/H17 rule
     if '/' in move:
         if int(game.dealer.config['HIT_ON_SOFT_17']) == 1:
             move = move.split('/')[0]
         elif int(game.dealer.config['HIT_ON_SOFT_17']) == 0:
             move = move.split('/')[1]
+    # evaluate possible surrender moves (since this function is used later on during play, not at the surrendering stage it changes to S/H)
     if move == 'Us':
         return 'S'
     elif move == 'Uh':
         return 'H'
-    elif move == 'Dh':
-        if (player.counted_hand_sums[hand_idx] < int(game.config['MIN_DOUBLE_DOWN_SUM'])) or (player.counted_hand_sums[hand_idx] > int(game.config['MAX_DOUBLE_DOWN_SUM'])):
-            return 'H'
-        elif (len(player.hands) > 1) and (int(game.config['DOUBLE_AFTER_SPLIT']) == 0):
-            return 'H'
-        elif len(player.hands[hand_idx]) > 2:
-            return 'H'
-        else:
-            return 'D'
-    elif move == 'Ds':
-        if (player.counted_hand_sums[hand_idx] < int(game.config['MIN_DOUBLE_DOWN_SUM'])) or (player.counted_hand_sums[hand_idx] > int(game.config['MAX_DOUBLE_DOWN_SUM'])):
-            return 'S'
-        elif (len(player.hands) > 1) and (int(game.config['DOUBLE_AFTER_SPLIT']) == 0):
-            return 'S'
-        elif len(player.hands[hand_idx]) > 2:
-            return 'S'
-        else:
-            return 'D'
     elif move == 'Usp':
-        return  'SP'
+        return 'SP'
+    elif (move == 'Dh') or (move == 'Ds'):
+        # do not allow double down if the sum is restricted in any way by game rules
+        if (player.counted_hand_sums[hand_idx] < int(game.config['MIN_DOUBLE_DOWN_SUM'])) or (player.counted_hand_sums[hand_idx] > int(game.config['MAX_DOUBLE_DOWN_SUM'])):
+            if 'h' in move:
+                return 'H'
+            elif 's' in move:
+                return 'S'
+        # do not allow double down if player splitted and its not allowed after split
+        elif (len(player.hands) > 1) and (int(game.config['DOUBLE_AFTER_SPLIT']) == 0):
+            if 'h' in move:
+                return 'H'
+            elif 's' in move:
+                return 'S'
+        # ensure that double down is only possible as the only move on hand
+        elif len(player.hands[hand_idx]) > 2:
+            if 'h' in move:
+                return 'H'
+            elif 's' in move:
+                return 'S'
+        else:
+            return 'D'
 
     return move
 
